f0b53af15a51686223bc51c39bfea70c29c443f3
Julia Picinini <C23418@bruning.com.br>
2025-02-03 09:50:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+alert('Boas vindas ao nosso site!');
+let nome = 'Lua';
+let idade = 25
+
+let numeroDeVendas = 50
+let saldoDisponivel = 1000
+
+let mensagemErro= prompt('Erro! Preencha todos os campos');
+
+alert(mensagemErro);
+
+let nome2 = prompt('Preencha com um nome ');
+let idade2 = prompt('Preencha com a sua idade');
+
+if(idade >= 18){
+    alert('Pode tirar a habilitação!');
+};
